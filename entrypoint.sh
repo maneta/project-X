bf83eabff46fdd95fce2e75db2ce262404e3a500
Daniel Cesario <maneta@gmail.com>
2017-09-09 13:46:15 +0200

    forcing morbo to use IPv4 binds (Morbo execution was broken)
    the base builddep stretch image comes with IPv6 disabled so morbo was
    failing to bind. So just using IPv4.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 if [ ${ENVIRONMENT} == "devel" ]; then
-  exec carton exec morbo -l http://[::]:8080 "$@"
+  exec carton exec morbo -l http://[0.0.0.0]:8080 "$@"
 else
   exec carton exec hypnotoad -f "$@"
 fi
